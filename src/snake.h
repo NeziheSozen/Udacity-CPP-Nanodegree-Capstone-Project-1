de657a6235189e1d9c6e48f9e393125ad499a29b
Nezihe Sözen <nezihe.sozen@gmail.com>
2023-05-01 21:42:29 +0300

    Update snake.h

--- src/snake.h
+++ src/snake.h
@@ -2,39 +2,53 @@
 #define SNAKE_H
 
 #include <vector>
+#include <algorithm>
+#include <mutex>
+#include <future>
+
 #include "SDL.h"
+
+struct SNAKE_MOVEMENT {
+  int x = 0;
+  int y = 0;
+};
 
 class Snake {
  public:
   enum class Direction { kUp, kDown, kLeft, kRight };
 
-  Snake(int grid_width, int grid_height)
-      : grid_width(grid_width),
-        grid_height(grid_height),
-        head_x(grid_width / 2),
-        head_y(grid_height / 2) {}
-
+  Snake(int grid_width, int grid_height);
+  ~Snake() {};
   void Update();
 
   void GrowBody();
-  bool SnakeCell(int x, int y);
-
-  Direction direction = Direction::kUp;
-
-  float speed{0.1f};
-  int size{1};
-  bool alive{true};
+  bool IsOnACell(int x, int y);
+  void SetDirection(const Direction direction);
+  void SetSpeed(const int speed);
+  Direction GetDirection() const { return snake_direction; };
+  int GetSpeed() const { return snake_speed; };
+  int GetSize() const { return snake_size; };
+  bool IsAlive() const { return is_snake_alive; };
+  float GetHeadX();
+  float GetHeadY();
+  void SetHeadX(const int x);
+  void SetHeadY(const int y);
+  std::vector<SDL_Point> GetBody();
+  void UpdateHead();
+  
+ private:
+  bool is_snake_growing = false;
+  int grid_width;
+  int grid_height;
+  float snake_speed =1.0;
+  int snake_size =1;
+  bool is_snake_alive = true;
   float head_x;
   float head_y;
   std::vector<SDL_Point> body;
-
- private:
-  void UpdateHead();
+  Direction snake_direction = Direction::kUp;
+  std::mutex snake_mutex;
   void UpdateBody(SDL_Point &current_cell, SDL_Point &prev_cell);
-
-  bool growing{false};
-  int grid_width;
-  int grid_height;
 };
 
 #endif
