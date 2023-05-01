7726cdcea0f5e552142ef30b71958fb32b96c01c
Nezihe Sözen <nezihe.sozen@gmail.com>
2023-05-01 21:45:20 +0300

    Update controller.h

--- src/controller.h
+++ src/controller.h
@@ -1,15 +1,16 @@
 #ifndef CONTROLLER_H
 #define CONTROLLER_H
 
+#include <mutex>
 #include "snake.h"
 
 class Controller {
  public:
-  void HandleInput(bool &running, Snake &snake) const;
+  void HandleInput(bool &running, Snake &snake);
 
  private:
-  void ChangeDirection(Snake &snake, Snake::Direction input,
-                       Snake::Direction opposite) const;
+  void ChangeDirection(Snake &snake, Snake::Direction input, Snake::Direction opposite) ;
+  std::mutex controller_mutex;
 };
 
 #endif
