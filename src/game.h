51fc9c6593dfe0db6b63bb3ddc99168baa46b1a8
Nezihe Sözen <nezihe.sozen@gmail.com>
2023-05-01 21:44:26 +0300

    Update game.h

--- src/game.h
+++ src/game.h
@@ -6,25 +6,29 @@
 #include "controller.h"
 #include "renderer.h"
 #include "snake.h"
+#include "search.h"
 
 class Game {
  public:
   Game(std::size_t grid_width, std::size_t grid_height);
-  void Run(Controller const &controller, Renderer &renderer,
-           std::size_t target_frame_duration);
+  void Run(Renderer &renderer, std::size_t target_frame_duration);
   int GetScore() const;
   int GetSize() const;
 
  private:
   Snake snake;
+  Controller controller;
   SDL_Point food;
+  std::unique_ptr<Search> search;
+  std::size_t grid_width;
+  std::size_t grid_height;
 
-  std::random_device dev;
+  std::random_device random_device;
   std::mt19937 engine;
   std::uniform_int_distribution<int> random_w;
   std::uniform_int_distribution<int> random_h;
 
-  int score{0};
+  int game_score = 0;
 
   void PlaceFood();
   void Update();
